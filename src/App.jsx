d7cfac359cebadd66f05a614e386f856dcf4d2c4
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-12-23 12:52:36 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 // import ProductList from "./features/product-list/ProductList.jsx";
 import Home from "./pages/Home.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  // Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "register",
+    element: <SignupPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
