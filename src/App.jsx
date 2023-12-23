7725589e36d3fb86e3994772e80196fffc41a6ee
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-12-23 12:09:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import ProductList from "./features/product-list/ProductList.jsx";
+import Home from "./pages/Home.jsx";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
